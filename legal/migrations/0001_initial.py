d5b2602f92152b9862a020f6d90cb50fb94ea304
rugala <adam@rugala.pl>
2017-01-25 03:51:32 -0800

    Update south migration to new style one

--- legal/migrations/0001_initial.py
+++ legal/migrations/0001_initial.py
@@ -1,107 +1,62 @@
 # -*- coding: utf-8 -*-
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
 
 
-try:
-    from django.contrib.auth import get_user_model
+class Migration(migrations.Migration):
 
-    User = get_user_model()
-except ImportError:
-    from django.contrib.auth.models import User
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
-user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
-
-
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'Agreement'
-        db.create_table('legal_agreement', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50, db_index=True)),
-        ))
-        db.send_create_signal('legal', ['Agreement'])
-
-        # Adding model 'AgreementVersion'
-        db.create_table('legal_agreementversion', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('agreement', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['legal.Agreement'])),
-            ('date', self.gf('django.db.models.fields.DateTimeField')()),
-            ('content', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal('legal', ['AgreementVersion'])
-
-        # Adding unique constraint on 'AgreementVersion', fields ['agreement', 'date']
-        db.create_unique('legal_agreementversion', ['agreement_id', 'date'])
-
-        # Adding model 'AgreementAcceptance'
-        db.create_table('legal_agreementacceptance', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm[user_orm_label])),
-            ('agreement_version',
-             self.gf('django.db.models.fields.related.ForeignKey')(to=orm['legal.AgreementVersion'])),
-            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-        ))
-        db.send_create_signal('legal', ['AgreementAcceptance'])
-
-        # Adding unique constraint on 'AgreementAcceptance', fields ['user', 'agreement_version', 'date']
-        db.create_unique('legal_agreementacceptance', ['user_id', 'agreement_version_id', 'date'])
-
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'AgreementAcceptance', fields ['user', 'agreement_version', 'date']
-        db.delete_unique('legal_agreementacceptance', ['user_id', 'agreement_version_id', 'date'])
-
-        # Removing unique constraint on 'AgreementVersion', fields ['agreement', 'date']
-        db.delete_unique('legal_agreementversion', ['agreement_id', 'date'])
-
-        # Deleting model 'Agreement'
-        db.delete_table('legal_agreement')
-
-        # Deleting model 'AgreementVersion'
-        db.delete_table('legal_agreementversion')
-
-        # Deleting model 'AgreementAcceptance'
-        db.delete_table('legal_agreementacceptance')
-
-
-    models = {
-        user_model_label: {
-            'Meta': {
-                'object_name': User.__name__,
-                'db_table': "'%s'" % User._meta.db_table
+    operations = [
+        migrations.CreateModel(
+            name='Agreement',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=50, db_index=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='AgreementAcceptance',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'ordering': ['-date'],
             },
-            User._meta.pk.attname: (
-                'django.db.models.fields.AutoField', [],
-                {'primary_key': 'True',
-                 'db_column': "'%s'" % User._meta.pk.column}
-            ),
-        },
-        'legal.agreement': {
-            'Meta': {'object_name': 'Agreement'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': (
-                'django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'})
-        },
-        'legal.agreementacceptance': {
-            'Meta': {'ordering': "['-date']", 'unique_together': "(('user', 'agreement_version', 'date'),)",
-                     'object_name': 'AgreementAcceptance'},
-            'agreement_version': (
-                'django.db.models.fields.related.ForeignKey', [], {'to': "orm['legal.AgreementVersion']"}),
-            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['%s']" % user_orm_label})
-        },
-        'legal.agreementversion': {
-            'Meta': {'ordering': "['-date']", 'unique_together': "(('agreement', 'date'),)",
-                     'object_name': 'AgreementVersion'},
-            'agreement': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['legal.Agreement']"}),
-            'content': ('django.db.models.fields.TextField', [], {}),
-            'date': ('django.db.models.fields.DateTimeField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
-        }
-    }
-
-    complete_apps = ['legal']
+        ),
+        migrations.CreateModel(
+            name='AgreementVersion',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField()),
+                ('content', models.TextField()),
+                ('agreement', models.ForeignKey(to='legal.Agreement')),
+            ],
+            options={
+                'ordering': ['-date'],
+            },
+        ),
+        migrations.AddField(
+            model_name='agreementacceptance',
+            name='agreement_version',
+            field=models.ForeignKey(to='legal.AgreementVersion'),
+        ),
+        migrations.AddField(
+            model_name='agreementacceptance',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AlterUniqueTogether(
+            name='agreementversion',
+            unique_together=set([('agreement', 'date')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='agreementacceptance',
+            unique_together=set([('user', 'agreement_version', 'date')]),
+        ),
+    ]
